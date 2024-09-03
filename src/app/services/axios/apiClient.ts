f6f91077f7663baf2e7e6c18bc6ba9fb5e2d1301
rkatarine.dev <rkatarine.dev@gmail.com>
2024-09-03 22:12:30 +0300

    add server

--- src/app/services/axios/apiClient.ts
+++
@@ -1,9 +0,0 @@
-import {Api} from "@/src/.api-client";
-import {apiConfig} from "./config";
-import {attachInterceptors} from "./interceptors/attachInterceptors";
-
-export const apiClient = new Api({
-    ...apiConfig,
-    attachInterceptors
-})
-
